fdedd770575f07151f35d391e4d345197346467d
Mark Alston <malston@pivotal.io>
2020-03-30 14:53:57 -0600

    Add script to create AD service account for bosh service principal

--- README.md
+++ README.md
@@ -13,12 +13,13 @@ export AZURE_REGION=<azure region>
 export AZURE_TENANT_ID=<azure tenant it>
 export AZURE_SUBSCRIPTION_ID=<azure subscription id>
 export AZURE_STORAGE_ACCOUNT_KEY=<azure storage account key>
+export ENVIRONMENT_NAME=controlplane
 EOF
 ```
 
 Run the following source command to set the environment variables into your shell or install [direnv](https://direnv.net/) to do this automatically.
 
-```
+```sh
 source .envrc
 ```
 
@@ -26,8 +27,9 @@ source .envrc
 
 - Run `./scripts/install-cli-tools.sh` to install required CLI tools
 - Update `./versions.yml` to use latest versions
-- Update `./control-plane/vars/$ENVIRONMENT_NAME/terraform.tfvars`
-- run `./scripts/terraform-control-plane-apply.sh` - this will create the
+- Follow [these instructions](https://docs.pivotal.io/platform/ops-manager/2-8/azure/prepare-azure-terraform.html#install) to create and configure the Service Principal account that is needed to run the terraform templates. To save time, you can run `./scripts/create-service-account.sh`
+- Copy `./control-plane/vars/$ENVIRONMENT_NAME/terraform.tfvars.example` to `./control-plane/vars/$ENVIRONMENT_NAME/terraform.tfvars` and modify with your configuration choices and credentials.
+- Run `./scripts/terraform-control-plane-apply.sh` - this will create the
   infrastructure required in Azure for a control-plane.
 - Update `opsman.yml` and `director.yml` in the control-plane vars directory.
 - Run `./scripts/download-opsman.yml`
---
+++ scripts/create-service-account.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+## Automates the creation of the service account necessary for terraforming
+## See https://docs.pivotal.io/platform/ops-manager/2-8/azure/prepare-azure-terraform.html#install
+
+set -e
+# only exit with zero if all commands of the pipeline exit successfully
+set -o pipefail
+
+
+PASSWORD=$(openssl rand -base64 32)
+SUBSCRIPTION_ID="$1"
+TENANT_ID=$2
+UNIQUE_ID_URI=${3}
+DISPLAY_NAME=${4:-"Service Principal for BOSH"}
+
+if [[ -z "$SUBSCRIPTION_ID" ]]; then
+	echo "Enter subscription id:"
+	read -r SUBSCRIPTION_ID
+fi
+
+if [[ -z "$TENANT_ID" ]]; then
+	echo "Enter tenant id:"
+	read -r TENANT_ID
+fi
+
+if [[ -z "$UNIQUE_ID_URI" ]]; then
+	echo "Enter identifier uri"
+	read -r UNIQUE_ID_URI
+fi
+
+echo "Creating app ${DISPLAY_NAME}"
+APP_ID=$(az ad app create --display-name "$DISPLAY_NAME" \
+  --password "$PASSWORD" --homepage "$UNIQUE_ID_URI" \
+  --identifier-uris "$UNIQUE_ID_URI" | jq -r .appId)
+
+ASSIGNEE_ID=$(az ad sp create --id "$APP_ID" | jq -r .objectId)
+
+# eventual consistency... retry until it works
+until az role assignment create --assignee "$ASSIGNEE_ID" \
+  --role "Contributor" --scope /subscriptions/"$SUBSCRIPTION_ID" > /dev/null 2>&1;
+do
+	echo 'Retrying role assignment...'
+	sleep 1
+done
+
+echo "Verifying role assignment to ${ASSIGNEE_ID}..."
+az role assignment list --assignee "$ASSIGNEE_ID"
+
+echo "Logging in as ${APP_ID}..."
+az login --username "$APP_ID" --password "$PASSWORD" \
+  --service-principal --tenant "$TENANT_ID" > /dev/null 2>&1;
+
+echo "To register your subscription with Microsoft.Storage, run:"
+echo
+echo "az provider register --namespace Microsoft.Storage"
+echo
+echo
+echo "To register your subscription with Microsoft.Network, run:"
+echo
+echo "az provider register --namespace Microsoft.Network"
+echo
+echo
+echo "To register your subscription with Microsoft.Compute, run:"
+echo
+echo "az provider register --namespace Microsoft.Compute"
+echo
+
+echo
+echo "Done!"
+echo
+echo "subscription id: ${SUBSCRIPTION_ID}"
+echo "tenant id: ${TENANT_ID}"
+echo "client id: ${APP_ID}"
+echo "client secret: ${PASSWORD}"
+echo
