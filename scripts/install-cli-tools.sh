afdb687f957dce9f2637fb1e3b1cb384924c0108
Mark Alston <malston@pivotal.io>
2020-04-02 16:22:45 -0600

    Update install script for cli tools

--- scripts/install-cli-tools.sh
+++ scripts/install-cli-tools.sh
@@ -12,11 +12,11 @@ function install_terraform {
   sudo mv terraform /usr/local/bin/terraform
   rm $file
   type terraform
-  terraform --version 
+  terraform --version
 }
 
 function install_pivnet_cli {
-  version="${PIVNET_VERSION:-1.0.1}"
+  version="${PIVNET_VERSION:-1.0.2}"
   os="${OS:-linux}"
   arch="${ARCH:-amd64}"
   file="pivnet"
@@ -73,12 +73,12 @@ function install_credhub {
   credhub --version
 }
 
-function install_pks_cli {
-  version="${PKS_VERSION:-1.6.0}"
+function install_k8s_clis {
+  version="${PKS_VERSION:-1.6.1}"
   os="${OS:-linux}"
   arch="${ARCH:-amd64}"
 
-  pivnet login --api-token $PIVNET_TOKEN
+  pivnet login --api-token "$PIVNET_TOKEN"
 
   pivnet download-product-files -p pivotal-container-service -r ${version} -g "pks-${os}-${arch}*"
   pivnet download-product-files -p pivotal-container-service -r ${version} -g "kubectl-${os}-${arch}*"
@@ -86,6 +86,7 @@ function install_pks_cli {
   chmod +x pks*
   sudo mv pks-$os-* /usr/local/bin/pks
   type pks
+  pks --version
   chmod +x kubectl-$os-*
   sudo mv kubectl* /usr/local/bin/kubectl
   type kubectl
@@ -132,6 +133,66 @@ function install_minio_client {
   mc --version
 }
 
+function install_kpack_logs {
+  version="${KPACK_VERSION:-v0.0.8}"
+  os="${OS:-linux}"
+  if [[ $os == darwin ]]; then
+    os="macos"
+  fi
+  file="logs.tgz"
+  trap "{ rm -f $file ; exit 255; }" EXIT
+  wget -O $file https://github.com/pivotal/kpack/releases/download/${version}/logs-${version}-${os}.tgz
+  tar -xvf $file
+  rm $file
+  chmod +x logs
+  sudo mv logs /usr/local/bin/logs
+  type logs
+}
+
+function install_ytt {
+  version="${YTT_VERSION:-0.26.0}"
+  os="${OS:-linux}"
+  arch="${ARCH:-amd64}"
+
+  pivnet login --api-token "$PIVNET_TOKEN"
+
+  pivnet download-product-files --product-slug='ytt' --release-version="$version" --glob=ytt-${os}-${arch}*
+
+  chmod +x ytt*
+  sudo mv ytt-$os-* /usr/local/bin/ytt
+  type ytt
+  ytt version
+}
+
+function install_kapp {
+  version="${KAPP_VERSION:-0.22.0}"
+  os="${OS:-linux}"
+  arch="${ARCH:-amd64}"
+
+  pivnet login --api-token "$PIVNET_TOKEN"
+
+  pivnet download-product-files --product-slug='kapp' --release-version="$version" --glob=kapp-${os}-${arch}*
+
+  chmod +x kapp*
+  sudo mv kapp-$os-* /usr/local/bin/kapp
+  type kapp
+  kapp version
+}
+
+function install_kbld {
+  version="${KBLD_VERSION:-0.14.0}"
+  os="${OS:-linux}"
+  arch="${ARCH:-amd64}"
+
+  pivnet login --api-token "$PIVNET_TOKEN"
+  pivnet download-product-files --product-slug='kbld' --release-version="$version" --glob=kbld-${os}-${arch}*
+
+  chmod +x kbld*
+  sudo mv kbld-$os-* /usr/local/bin/kbld
+  type kbld
+  kbld version
+}
+
 set -eou pipefail
 
 echo "Enter your Operating System (linux, darwin, windows): "
@@ -140,8 +201,10 @@ read -r OS
 echo "Enter your OS Architecture (amd64, 386): "
 read -r ARCH
 
-echo "Enter your Pivnet refresh token: "
-read -rs PIVNET_TOKEN
+if [ -z "$PIVNET_TOKEN" ]; then
+  echo "Enter your Pivnet refresh token: "
+  read -rs PIVNET_TOKEN
+fi
 
 if [[ $OS == linux ]]; then
   sudo apt install zip -y
@@ -152,7 +215,11 @@ install_pivnet_cli
 install_jq
 install_om
 install_credhub
-install_pks_cli
+install_k8s_clis
 install_helm_cli
 install_bosh_cli
 install_minio_client
+install_kpack_logs
+install_ytt
+install_kapp
+install_kbld
