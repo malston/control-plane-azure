de4040a682e3ce6e7d62fbef333a454b92b6ba59
Mark Alston <malston@pivotal.io>
2020-04-02 10:05:19 -0600

    Cleanup terraform scripts

--- scripts/terraform-control-plane-apply.sh
+++ scripts/terraform-control-plane-apply.sh
@@ -4,22 +4,29 @@ set -e
 # only exit with zero if all commands of the pipeline exit successfully
 set -o pipefail
 
+__DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 if [ -z "$ENVIRONMENT_NAME" ]; then
     echo "Must provide environment name ENVIRONMENT_NAME as environment variable"
     echo "Set this to the same value of environment_name var in terraform.tfvars"
     exit 1
 fi
 
-mkdir -p "./control-plane/state/${ENVIRONMENT_NAME}"
+terraform_dir="${__DIR}/../control-plane/terraform"
 
-terraform_dir=./control-plane/terraform
+state_dir="${__DIR}/../control-plane/state/$ENVIRONMENT_NAME"
+vars_dir="${__DIR}/../control-plane/vars/$ENVIRONMENT_NAME"
 
-pushd ${terraform_dir} > /dev/null
+mkdir -p "${state_dir}"
+mkdir -p "${vars_dir}"
+
+pushd "${terraform_dir}" > /dev/null
   terraform init
-  terraform plan -var-file="../vars/${ENVIRONMENT_NAME}/terraform.tfvars" \
-    -out="../state/${ENVIRONMENT_NAME}/terraform.tfplan"
+  terraform plan -var-file="${vars_dir}/terraform.tfvars" \
+    -out="${state_dir}/terraform.tfplan"
   terraform apply \
-    -state="../state/${ENVIRONMENT_NAME}/terraform.tfstate" \
-     "../state/${ENVIRONMENT_NAME}/terraform.tfplan"
-  terraform output
+    -state="${state_dir}/terraform.tfstate" \
+    "${state_dir}/terraform.tfplan"
 popd > /dev/null
+
+terraform output -state="${state_dir}/terraform.tfstate"
--- scripts/terraform-control-plane-destroy.sh
+++ scripts/terraform-control-plane-destroy.sh
@@ -4,17 +4,22 @@ set -e
 # only exit with zero if all commands of the pipeline exit successfully
 set -o pipefail
 
+__DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 if [ -z "$ENVIRONMENT_NAME" ]; then
     echo "Must provide environment name ENVIRONMENT_NAME as environment variable"
     echo "Set this to the same value of environment_name var in terraform.tfvars"
     exit 1
 fi
 
-mkdir -p "./control-plane/state/${ENVIRONMENT_NAME}"
+state_dir="${__DIR}/../control-plane/state/$ENVIRONMENT_NAME"
+vars_dir="${__DIR}/../control-plane/vars/$ENVIRONMENT_NAME"
 
-terraform_dir=./control-plane/terraform
+terraform_dir="${__DIR}/../control-plane/terraform"
 
-pushd ${terraform_dir} > /dev/null
-  terraform destroy -var-file=../vars/${ENVIRONMENT_NAME}/terraform.tfvars \
-    -state=../state/${ENVIRONMENT_NAME}/terraform.tfstate
+pushd "${terraform_dir}" > /dev/null
+  terraform destroy -var-file="${vars_dir}/terraform.tfvars" \
+    -state="${state_dir}/terraform.tfstate"
 popd > /dev/null
+
+rm -rf "${state_dir}"
